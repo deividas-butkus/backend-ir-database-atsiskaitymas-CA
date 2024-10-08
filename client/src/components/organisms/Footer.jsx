edf8b6eaf08553fbef04a5252daad45b7520940e
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-08 14:58:59 +0300

    Footer done

--- client/src/components/organisms/Footer.jsx
+++ client/src/components/organisms/Footer.jsx
@@ -1,18 +1,88 @@
 import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { Link } from "react-router-dom";
 
 const StyledFooters = styled.footer`
-  padding: 0 10%;
-  height: 100px;
+  min-height: 20vh;
+  padding: 10px 10%;
   background-color: #ccc;
   color: #000;
+  display: flex;
+  justify-content: space-between;
+  > div {
+    a {
+      color: inherit;
+      text-decoration: none;
+      &:hover {
+        color: #d73f03;
+      }
+    }
+  }
+  > div.social {
+    display: flex;
+    gap: 10px;
+    margin-top: 16px;
+  }
 `;
 
 const Footer = () => {
   return (
     <StyledFooters>
-      <span>Footer</span>
+      <div>
+        <p>Nowhere Public Library</p>
+        <p>&copy; {new Date().getFullYear()}</p>
+      </div>
+      <div>
+        <p>Working hours: 24/7</p>
+        <p>Address: somwhere at Kalvariju Market</p>
+      </div>
+      <div>
+        {/* Changed the <p> to a <div> for the iframe */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17555.6401067273!2d25.262712836265575!3d54.7003215887678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96a9e18ad90f%3A0x24f9751bf4c2e79e!2sKalvarij%C5%B3%20turgus!5e1!3m2!1slt!2slt!4v1728387658500!5m2!1slt!2slt"
+          width="250"
+          height="200"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div>
+        <p>
+          <Link to={"/policies/privacy"}>Privacy pociy</Link>
+        </p>
+        <p>
+          <Link to={"/policies/cookies"}>Cookies</Link>
+        </p>
+        <p>
+          <Link to={"/policies/terms"}>Terms of Use</Link>
+        </p>
+      </div>
+      <div className="social">
+        <a href="https://www.facebook.com/">
+          <FacebookIcon />
+        </a>
+        <a href="https://www.youtube.com/">
+          <YouTubeIcon />
+        </a>
+        <a href="https://www.instagram.com/">
+          <InstagramIcon />
+        </a>
+      </div>
     </StyledFooters>
   );
 };
 
 export default Footer;
+
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17555.6401067273!2d25.262712836265575!3d54.7003215887678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96a9e18ad90f%3A0x24f9751bf4c2e79e!2sKalvarij%C5%B3%20turgus!5e1!3m2!1slt!2slt!4v1728387658500!5m2!1slt!2slt"
+  width="600"
+  height="450"
+  style="border:0;"
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade"
+></iframe>;
